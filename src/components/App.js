3f1d80d8442d348c49d263b435d45f66b4b0256b
Shivam Patidar <125869590+PROShivamPatidar@users.noreply.github.com>
2024-01-18 14:07:26 +0000

    yuyuy

--- src/components/App.js
+++ src/components/App.js
@@ -4,6 +4,7 @@ import '../styles/App.css';
 const App = () => {
   return (
     <div>
+      <h1>Welcome to My React App</h1>
       <p>I am learning React. My life is getting better.</p>
     </div>
   )
